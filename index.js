b80dcb4a433783631784702d181a578fdaab194b
Gavri (Gabriel) Guy <gavri.guy@gmail.com>
2017-03-16 22:21:26 +0200

    remove host

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: process.env.PORT || 3000, host: 'localhost' });
+server.connection({ port: process.env.PORT || 3000 });
 
 server.route({
     method: 'GET',
